7a556cb6ed873824a49588ff6f6681a76f72b140
Sabrina Accime <72281709+sabrinaaccime@users.noreply.github.com>
2024-11-30 15:08:30 -0800

    fix(ui): add svgs and paths for missing icons

--- src/app/components/Form.jsx
+++ src/app/components/Form.jsx
@@ -1,29 +1,40 @@
-
-'use client';
+"use client";
 
 function Form() {
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      // reset the form
-      event.target.reset();
-    };
-    return (
-      <form className="form" onSubmit={handleSubmit}>
-        <label htmlFor="todo">
-          <input
-            type="text"
-            name="todo"
-            id="todo"
-            placeholder="Write your next task"
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // reset the form
+    event.target.reset();
+  };
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <label htmlFor="todo">
+        <input
+          type="text"
+          name="todo"
+          id="todo"
+          placeholder="Write your next task"
+        />
+      </label>
+      <button>
+        <span className="visually-hidden">Submit</span>
+        <svg
+          clipRule="evenodd"
+          fillRule="evenodd"
+          strokeLinejoin="round"
+          strokeMiterlimit="2"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          width={32}
+          height={32}
+        >
+          <path
+            d="m11 11h-7.25c-.414 0-.75.336-.75.75s.336.75.75.75h7.25v7.25c0 .414.336.75.75.75s.75-.336.75-.75v-7.25h7.25c.414 0 .75-.336.75-.75s-.336-.75-.75-.75h-7.25v-7.25c0-.414-.336-.75-.75-.75s-.75.336-.75.75z"
+            fillRule="nonzero"
           />
-        </label>
-        <button>
-          <span className="visually-hidden">Submit</span>
-          <svg>
-            <path d="" />
-          </svg>
-        </button>
-      </form>
-    );
-  }
-  export default Form;
+        </svg>
+      </button>
+    </form>
+  );
+}
+export default Form;
