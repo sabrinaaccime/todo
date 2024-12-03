4d801878645b49521ce63c763c13f186d42973a5
Sabrina Accime <72281709+sabrinaaccime@users.noreply.github.com>
2024-12-02 20:03:04 -0800

    feat: implement state sharing to allow user to add a new task and see how many tasks they have in total

--- src/app/components/Form.jsx
+++ src/app/components/Form.jsx
@@ -1,9 +1,13 @@
 "use client";
 
-function Form() {
+function Form({ setTodos }) {
   const handleSubmit = (event) => {
     event.preventDefault();
-    // reset the form
+    const value = event.target.todo.value;
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { title: value, id: self.crypto.randomUUID(), is_completed: false },
+    ]);
     event.target.reset();
   };
   return (
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import Form from "./components/Form";
 import Header from "./components/Header";
@@ -5,12 +7,17 @@ import TODOHero from "./components/TODOHero";
 import TODOList from "./components/TODOList";
 
 function Home() {
+  const [todos, setTodos] = React.useState([]);
+  const todos_completed = todos.filter(
+    (todo) => todo.is_completed === true
+  ).length;
+  const total_todos = todos.length;
   return (
     <div className="wrapper">
       <Header />
-      <TODOHero todos_completed={0} total_todos={0} />
-      <Form />
-      <TODOList todos={[]} />
+      <TODOHero todos_completed={todos_completed} total_todos={total_todos} />
+      <Form todos={todos} setTodos={setTodos} />
+      <TODOList todos={todos} setTodos={setTodos} />
     </div>
   );
 }
