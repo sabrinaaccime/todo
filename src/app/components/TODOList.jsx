0494c244a02698e61c2eb02430ba6e8c9e5ab177
Sabrina Accime <72281709+sabrinaaccime@users.noreply.github.com>
2024-12-03 19:42:17 -0800

    feat: user is able to mark todos as complete

--- src/app/components/TODOList.jsx
+++ src/app/components/TODOList.jsx
@@ -12,14 +12,36 @@ function TODOList({ todos, setTodos }) {
   );
 }
 
-function Item({ item }) {
+function Item({ item, setTodos }) {
+  const completedTodo = () => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === item.id
+          ? { ...todo, is_completed: !todo.is_completed }
+          : todo
+      )
+    );
+  };
   return (
     <li id={item?.id} className="todo_item">
-      <button className="todo_items_left">
-        <svg>
-          <circle cx="11.998" cy="11.998" fillRule="nonzero" r="9.998" />
+      <button className="todo_items_left" onClick={completedTodo}>
+        <svg
+          clipRule="evenodd"
+          fillRule="evenodd"
+          strokeLinejoin="round"
+          strokeMiterlimit="2"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          width={34}
+          height={34}
+          stroke="#22C55E"
+          fill={item.is_completed ? "#22C55E" : "0d0d0d"}
+        >
+          <circle cx="11.998" cy="11.998" fillRule="nonzero" r="9.998"></circle>
         </svg>
-        <p>{item?.title}</p>
+        <p style={item.is_completed ? { textDecoration: "line-through" } : {}}>
+          {item?.title}
+        </p>
       </button>
       <div className="todo_items_right">
         <button>
